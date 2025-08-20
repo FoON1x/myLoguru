39e5aa62218e8d3a6d7f41760aa8210acdf95897
Eric <eric34@163.com>
2025-08-20 14:54:43 +0800

    高级用法

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ def main():
 
 
 if __name__ == "__main__":
-    # logger.remove()
-    logger.add('msg.log', level='ERROR')
-    # logger.remove(handler)
+
 
     logger.trace('trace msg')
     logger.debug('debug msg')
@@ -16,3 +14,14 @@ if __name__ == "__main__":
     logger.warning('warning msg')
     logger.error('error msg')
     logger.critical('critical msg')
+
+    child = logger.bind(foo='bar')
+    child.info('info msg')
+    with logger.contextualize(programmer="want"):
+        logger.trace('trace msg')
+        logger.debug('debug msg')
+        logger.info('info msg')
+        logger.success('success msg')
+        logger.warning('warning msg')
+        logger.error('error msg')
+        logger.critical('critical msg')
